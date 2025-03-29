39f9a4edf1ff63b0a003e79f9c5613ec47d4a2ff
AVINASH <avinashverma06@yahoo.in>
2025-03-29 22:45:37 +0530

    Tested new branch

--- src/main/java/org/Selenium/Demo.java
+++ src/main/java/org/Selenium/Demo.java
@@ -1,13 +1,11 @@
 package org.Selenium;
 
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Wait;
+
 
 public class Demo {
     public static void main(String[] args) throws InterruptedException {
@@ -19,7 +17,7 @@ public class Demo {
         driver.manage().window().maximize();
         Thread.sleep(1000);
         driver.close();
-
+System.out.println(driver.getTitle());
 
     }
 }
